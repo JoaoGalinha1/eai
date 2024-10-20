00b0b0324f7796e473086c320f3e199ea0109cd3
JoaoGalinha1 <andrevalleestagio@gmail.com>
2024-10-20 12:30:35 -0300

    Update Untitled-2.js

--- Untitled-2.js
+++ Untitled-2.js
@@ -2,3 +2,15 @@
 function abrirPix() {
     window.open('pix.html', '_blank');
 }
+
+// Função para mostrar a lista de presentes
+function mostrarListaPresentes() {
+    document.getElementById("lista-presentes").style.display = "block";
+    document.getElementById("galerias").style.display = "none"; // Oculta galeria
+}
+
+// Função para mostrar a galeria
+function mostrarGaleria() {
+    document.getElementById("lista-presentes").style.display = "none"; // Oculta lista de presentes
+    document.getElementById("galerias").style.display = "block";
+}
