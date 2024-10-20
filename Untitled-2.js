a864148d8eccf258be0ed23debbfd8d4546f2e41
JoaoGalinha1 <andrevalleestagio@gmail.com>
2024-10-20 11:32:43 -0300

    Update Untitled-2.js

--- Untitled-2.js
+++ Untitled-2.js
@@ -1,4 +1,11 @@
-// Função para mostrar a chave PIX ao clicar no botão "presentear"
+// Função para mostrar o QR Code da chave PIX ao clicar no botão "presentear"
 function mostrarPix() {
-    alert("Chave PIX: andrevalleestagio@gmail.com");
+    var pixImg = document.createElement('img');
+    pixImg.src = 'pix.jfif'; // QR code da chave PIX
+    pixImg.style.width = '200px'; // Ajustar o tamanho do QR Code
+    pixImg.style.marginTop = '20px';
+
+    // Exibe o QR code na página
+    var body = document.querySelector('body');
+    body.appendChild(pixImg);
 }
