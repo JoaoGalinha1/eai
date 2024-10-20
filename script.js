ceb81bfa144b559f5ab45ac5f903324c9f55be68
JoaoGalinha1 <andrevalleestagio@gmail.com>
2024-10-20 12:58:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Função para alternar as seções
+function mostrarSecao(secao) {
+    var secoes = document.getElementsByClassName('secao');
+    for (var i = 0; i < secoes.length; i++) {
+        secoes[i].style.display = 'none';
+    }
+    document.getElementById(secao).style.display = 'block';
+}
+
 // Função para abrir uma nova página com o QR Code para PIX
 function abrirPix() {
     window.open('pix.html', '_blank');
