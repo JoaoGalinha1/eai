c17acd43bf3e677b49582e7378bf0f1f27ecef6b
JoaoGalinha1 <andrevalleestagio@gmail.com>
2024-10-20 11:44:18 -0300

    Update Untitled-2.js

--- Untitled-2.js
+++ Untitled-2.js
@@ -1,11 +1,4 @@
-// Função para mostrar o QR Code da chave PIX ao clicar no botão "presentear"
-function mostrarPix() {
-    var pixImg = document.createElement('img');
-    pixImg.src = 'pix.jfif'; // QR code da chave PIX
-    pixImg.style.width = '200px'; // Ajustar o tamanho do QR Code
-    pixImg.style.marginTop = '20px';
-
-    // Exibe o QR code na página
-    var body = document.querySelector('body');
-    body.appendChild(pixImg);
+// Função para abrir uma nova página com o QR Code para PIX
+function abrirPix() {
+    window.open('pix.html', '_blank');
 }
