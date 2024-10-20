595d37529cf7b838f93dc05e57d58d38b1f6b2ae
JoaoGalinha1 <andrevalleestagio@gmail.com>
2024-10-20 12:20:07 -0300

    Update Untitled-2.js

--- Untitled-2.js
+++ Untitled-2.js
@@ -2,18 +2,3 @@
 function abrirPix() {
     window.open('pix.html', '_blank');
 }
-function mostrarSecao(secao) {
-    // Ocultar todas as seções
-    var secaoAtual = document.querySelectorAll('.section');
-    secaoAtual.forEach(function(s) {
-        s.style.display = 'none';
-    });
-
-    // Exibir a seção clicada
-    document.getElementById(secao).style.display = 'block';
-}
-
-// Função para abrir o PIX
-function abrirPix() {
-    window.open('pix.html', '_blank');
-}
